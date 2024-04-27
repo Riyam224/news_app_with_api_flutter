b1e4812c15034fa0ac6e784730b4ccce1e9db32a
Riyam224 <cs50riyam211#gmail.com>
2024-04-27 20:57:19 +0300

    add cached network image

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app_/models/article_model.dart';
 
@@ -13,28 +14,41 @@ class NewsList extends StatelessWidget {
       child: Column(
         children: [
           //
-          Image.network(
-            articles.image,
-            width: double.infinity,
-            height: 200,
-            fit: BoxFit.fill,
-          ),
-          Text(
-            articles.title,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            style: const TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: CachedNetworkImage(
+              imageUrl: articles.image,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Image.network(
+                'assets/images/image_not_found.png',
+                fit: BoxFit.cover,
+              ),
+              fit: BoxFit.cover,
             ),
           ),
-          Text(
-            articles.subTitle,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w300,
+          Padding(
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            child: Text(
+              articles.title,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            child: Text(
+              articles.subTitle,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: const TextStyle(
+                fontSize: 15,
+                color: Color(0xffb1afb0),
+                fontWeight: FontWeight.w300,
+              ),
             ),
           )
         ],
